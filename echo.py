7c9dafdcf1be2a5e61ad382cfc6da4be8e98bf15
BermetKalmakova <bermetkalmakova@gmail.com>
2017-10-02 23:14:57 -0400

    done

---
+++ echo.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+my_app = Flask(__name__)
+
+@my_app.route('/', methods = ['GET', 'POST'])
+def root():
+    return render_template('form.html') 
+
+@my_app.route('/greet', methods = ['POST'])
+def greet(): 
+    print request.form['username']
+    return render_template("greeting.html", input1 = request.form['username'])
+ 
+
+if __name__ == '__main__':
+    my_app.debug = True
+    my_app.run()
+
+    
